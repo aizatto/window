7999638df260214ae90b2c37f4cdcd9ef00ea259
Aizat Faiz <aizat.faiz@gmail.com>
2019-06-17 20:52:32 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useLayoutEffect } from 'react';
+import { Container, FormGroup, Label, Col, Table, Input } from 'reactstrap';
+import Navbar from './Navbar';
+
+import "./bootstrap.min.css";
 
 const App: React.FC = () => {
+  const [innerHeight, setInnerHeight] = useState(window.innerHeight);
+  const [innerWidth, setInnerWidth] = useState(window.innerWidth);
+  const [outerHeight, setOuterHeight] = useState(window.outerHeight);
+  const [outerWidth, setOuterWidth] = useState(window.outerWidth);
+
+  useEffect(() => {
+    const resizeListener = () => {
+      setInnerHeight(window.innerHeight);
+      setInnerWidth(window.innerWidth);
+      setOuterHeight(window.outerHeight);
+      setOuterWidth(window.outerWidth);
+    }
+
+    window.addEventListener('resize', resizeListener);
+    return () => {
+      window.removeEventListener('resize', resizeListener);
+    };
+  });
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Container>
+        <Table hover>
+          <thead>
+            <tr>
+              <th style={{width: 200}}>Field</th>
+              <th>Value</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <a href="https://developer.mozilla.org/en-US/docs/Web/API/Window/innerHeight">
+                  window.innerHeight
+                </a>
+              </td>
+              <td><code>{innerHeight}</code></td>
+            </tr>
+            <tr>
+              <td>
+                <a href="https://developer.mozilla.org/en-US/docs/Web/API/Window/innerWidth">
+                  window.innerWidth
+                </a>
+              </td>
+              <td><code>{innerWidth}</code></td>
+            </tr>
+            <tr>
+              <td>
+                <a href="https://developer.mozilla.org/en-US/docs/Web/API/Window/outerHeight">
+                  window.outerHeight
+                </a>
+              </td>
+              <td><code>{outerHeight}</code></td>
+            </tr>
+            <tr>
+              <td>
+                <a href="https://developer.mozilla.org/en-US/docs/Web/API/Window/outerWidth">
+                  window.outerWidth
+                </a>
+              </td>
+              <td><code>{outerWidth}</code></td>
+            </tr>
+          </tbody>
+        </Table>
+      </Container>
+    </>
   );
 }
 
---
+++ src/Navbar.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Nav,
+  Navbar as BootstrapNavbar,
+  NavbarBrand,
+  NavLink,
+  NavItem,
+  NavbarToggler,
+} from 'reactstrap';
+
+export default function Navbar() {
+  const [collapsed, setCollapsed] = useState(false);
+
+  return (
+    <BootstrapNavbar color="dark" dark expand="md">
+      <NavbarBrand href="https://www.aizatto.com">aizatto.com</NavbarBrand>
+      <NavbarToggler onClick={() => setCollapsed(!collapsed)} />
+      <Collapse isOpen={collapsed} navbar>
+        <Nav className="ml-auto" navbar>
+          <NavItem>
+            <NavLink href="https://www.aizatto.com/">aizatto.com</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="https://www.deepthoughtapp.com/">Deep Thought</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="https://www.logbook.my/">Logbook</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="https://github.com/aizatto/dom-window">GitHub</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="https://www.linkedin.com/in/aizatto">LinkedIn</NavLink>
+          </NavItem>
+        </Nav>
+      </Collapse>
+    </BootstrapNavbar>
+  );
+}
