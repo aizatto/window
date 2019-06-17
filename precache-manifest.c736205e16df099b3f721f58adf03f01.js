0f6434173451c2c80f05eb823c3fa2cca0e2baf1
Aizat Faiz <aizat.faiz@gmail.com>
2019-06-17 20:55:15 +0800

    Updates

---
+++ precache-manifest.c736205e16df099b3f721f58adf03f01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56201f021cb4ab7f3ef0bfed3e89cad8",
+    "url": "/window/index.html"
+  },
+  {
+    "revision": "fd91d128f869c08802e6",
+    "url": "/window/static/css/main.93d668ce.chunk.css"
+  },
+  {
+    "revision": "d2f196ba0980467471a4",
+    "url": "/window/static/js/2.7ef52151.chunk.js"
+  },
+  {
+    "revision": "fd91d128f869c08802e6",
+    "url": "/window/static/js/main.33a940b9.chunk.js"
+  },
+  {
+    "revision": "5f4cedaae02cabee7ac6",
+    "url": "/window/static/js/runtime~main.7821a939.js"
+  }
+]);
